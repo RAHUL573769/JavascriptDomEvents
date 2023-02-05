23009766de5045e80897a0490d2f857ea092d265
Rahul Rudra <rahulrudra665@gmail.com>
2023-02-05 09:02:02 +0600

    colors added

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const makeBlue=document.getElementById("makeBlue");
 makeBlue.onclick=function makeBlue(){
     document.body.style.backgroundColor="blue";
 }
+
+
+const makePurple=document.getElementById("makepurple");
+
+makePurple.addEventListener("click",()=>{
+    document.body.style.backgroundColor="purple";
+})
+
+const makeOrange=document.getElementById("makeorange");
+makeOrange.addEventListener("click",function(){
+    document.body.style.backgroundColor="orange";
+})
